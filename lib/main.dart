8b50e1fd8cd5a8ca24f9ca3c0f39ad82dae280b6
takemotomizuki <aya.greenapple0302@gmail.com>
2024-05-28 00:09:50 +0900

    ML kitの追加

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 8;
 
   void _incrementCounter() {
     setState(() {
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter+=9;
     });
   }
 
