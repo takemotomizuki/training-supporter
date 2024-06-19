844c8bfc8f6f90ecef2188d8105853296f45ce8d
tezizizi <tezitezi2super@gmail.com>
2024-06-20 02:39:36 +0900

    get data from firebase ... Todo: update data

--- lib/view/training_menu.dart
+++ lib/view/training_menu.dart
@@ -1,8 +1,16 @@
+import 'dart:ffi';
+import 'dart:math';
+
+// import 'package:app/view/firestore.dart';
 import 'package:app/view/training.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+// import 'firebase_options.dart';
 
 class TrainingMenu extends StatefulWidget {
-  const TrainingMenu({super.key, required this.title});
+  TrainingMenu({super.key, required this.title});
 
   final String title;
 
@@ -11,199 +19,248 @@ class TrainingMenu extends StatefulWidget {
 }
 
 class TrainingMenuState extends State<TrainingMenu> {
+  List<DocumentSnapshot> menus = [];
+  Future getTrainingMenu() async {
+    await Firebase.initializeApp();
+    final snapshot =
+        await FirebaseFirestore.instance.collection('TrainingMenu').get();
+    menus = snapshot.docs;
+    print("helllllllllll");
+    print(menus.length);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    const double height = 110;
     return Scaffold(
-      body: Container(
-        margin:
-            const EdgeInsets.only(left: 30, top: 100, right: 30, bottom: 50),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        // MaterialPageRoute(builder: (context) => Training(title: 'どうも',)),
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                TakePictureScreen(title: 'Pose Detector')),
-                      );
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(5),
-                      height: 110,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        color: Colors.orangeAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      alignment: Alignment.center,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          const SizedBox(
-                            height: 100,
-                            width: 120,
-                            child: Center(
-                              child: Text(
-                                "Squat\n30回",
-                                style: TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.bold),
-                              ),
+      body: FutureBuilder(
+        future: getTrainingMenu(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: Text("LOADING"),
+            );
+          } else {
+            return Container(
+              margin: const EdgeInsets.only(
+                  left: 30, top: 100, right: 30, bottom: 50),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: menus.map((menu) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              // MaterialPageRoute(builder: (context) => Training(title: 'どうも',)),
+                              MaterialPageRoute(
+                                  builder: (context) => const TakePictureScreen(
+                                      title: 'Pose Detector')),
+                              // builder: (context) => Firestor()),
+                            );
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.all(5),
+                            height: min(height, size.height),
+                            width: size.width - 70,
+                            decoration: BoxDecoration(
+                              color: Colors.orangeAccent,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            alignment: Alignment.center,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                SizedBox(
+                                  height: height,
+                                  width: size.width / 3,
+                                  child: Center(
+                                    child: Text(
+                                      "${menu['name']}\n ${menu['times']}",
+                                      style: const TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: height,
+                                  width: size.width / 3,
+                                  child: Center(
+                                    child: Image.asset('images/model1.jpg'),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          SizedBox(
-                            height: 100,
-                            width: 100,
-                            child: Image.asset('images/model1.jpg'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+                        ),
+                      ],
+                    );
+                  }).toList(),
+                ),
+                // children: <Widget>[
+                //   Row(
+                //     mainAxisAlignment: MainAxisAlignment.center,
+                //     children: [
+                //       GestureDetector(
+                //         onTap: () {
+                //           Navigator.push(
+                //             context,
+                //             // MaterialPageRoute(builder: (context) => Training(title: 'どうも',)),
+                //             MaterialPageRoute(
+                //                 // builder: (context) =>
+                //                 //     TakePictureScreen(title: 'Pose Detector')),
+                //                 builder: (context) => Firestore()),
+                //           );
+                //         },
+                //         child: Container(
+                //           margin: const EdgeInsets.all(5),
+                //           height: min(height, size.height),
+                //           width: size.width,
+                //           decoration: BoxDecoration(
+                //             color: Colors.orangeAccent,
+                //             borderRadius: BorderRadius.circular(10),
+                //           ),
+                //           alignment: Alignment.center,
+                //           child: Row(
+                //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //             children: <Widget>[
+                //               const SizedBox(
+                //                 height: 100,
+                //                 width: 120,
+                //                 child: Center(
+                //                   child: Text(
+                //                     "Squat\n30回",
+                //                     style: TextStyle(
+                //                         fontSize: 24, fontWeight: FontWeight.bold),
+                //                   ),
+                //                 ),
+                //               ),
+                //               SizedBox(
+                //                 height: 100,
+                //                 width: 100,
+                //                 child: Image.asset('images/model1.jpg'),
+                //               ),
+                //             ],
+                //           ),
+                //         ),
+                //       ),
+                //     ],
+                //   ),
+                //   Row(
+                //     mainAxisAlignment: MainAxisAlignment.center,
+                //     children: [
+                //       GestureDetector(
+                //         onTap: () {
+                //           MaterialPageRoute(
+                //               builder: (context) =>
+                //                   TakePictureScreen(title: 'Pose_Detector'));
+                //         },
+                //         child: Container(
+                //           margin: const EdgeInsets.all(5),
+                //           height: 110,
+                //           width: 300,
+                //           decoration: BoxDecoration(
+                //             color: Colors.orangeAccent,
+                //             borderRadius: BorderRadius.circular(10),
+                //           ),
+                //           alignment: Alignment.center,
+                //         ),
+                //       ),
+                //     ],
+                //   ),
+                //   Row(
+                //     mainAxisAlignment: MainAxisAlignment.center,
+                //     children: [
+                //       GestureDetector(
+                //         onTap: () {
+                //           MaterialPageRoute(
+                //               builder: (context) =>
+                //                   TakePictureScreen(title: 'Pose_Detector'));
+                //         },
+                //         child: Container(
+                //           margin: const EdgeInsets.all(5),
+                //           height: 110,
+                //           width: 300,
+                //           decoration: BoxDecoration(
+                //             color: Colors.orangeAccent,
+                //             borderRadius: BorderRadius.circular(10),
+                //           ),
+                //           alignment: Alignment.center,
+                //         ),
+                //       ),
+                //     ],
+                //   ),
+                // ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              TakePictureScreen(title: 'Pose_Detector'));
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(5),
-                      height: 110,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        color: Colors.orangeAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              TakePictureScreen(title: 'Pose_Detector'));
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(5),
-                      height: 110,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        color: Colors.orangeAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              TakePictureScreen(title: 'Pose_Detector'));
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(5),
-                      height: 110,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        color: Colors.orangeAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              TakePictureScreen(title: 'Pose_Detector'));
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(5),
-                      height: 110,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        color: Colors.orangeAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              TakePictureScreen(title: 'Pose_Detector'));
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(5),
-                      height: 110,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        color: Colors.orangeAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              TakePictureScreen(title: 'Pose_Detector'));
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(5),
-                      height: 110,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        color: Colors.orangeAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+            );
+          }
+        },
       ),
     );
   }
+
+//   Future<Column> getDatabase() async {
+//     List<DocumentSnapshot> documentList = [];
+//     // final Size size = MediaQuery.of(context).size;
+//     const double height = 110;
+//     // コレクション内のドキュメント一覧を表示
+//     return Column(
+//       children: documentList.map((document) {
+//         return Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             GestureDetector(
+//               onTap: () {
+//                 Navigator.push(
+//                   context,
+//                   // MaterialPageRoute(builder: (context) => Training(title: 'どうも',)),
+//                   MaterialPageRoute(
+//                       // builder: (context) =>
+//                       //     TakePictureScreen(title: 'Pose Detector')),
+//                       builder: (context) => Firestore()),
+//                 );
+//               },
+//               child: Container(
+//                 margin: const EdgeInsets.all(5),
+//                 height: min(height, size.height),
+//                 width: size.width,
+//                 decoration: BoxDecoration(
+//                   color: Colors.orangeAccent,
+//                   borderRadius: BorderRadius.circular(10),
+//                 ),
+//                 alignment: Alignment.center,
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: <Widget>[
+//                     SizedBox(
+//                       height: 100,
+//                       width: 120,
+//                       child: Center(
+//                         child: Text(
+//                           // name,
+//                           '${document['times']} \n ${document['times']}',
+//                           style: const TextStyle(
+//                               fontSize: 24, fontWeight: FontWeight.bold),
+//                         ),
+//                       ),
+//                     ),
+//                     SizedBox(
+//                       height: 100,
+//                       width: 100,
+//                       child: Image.asset('images/model1.jpg'),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ],
+//         );
+//       }).toList(),
+//     );
+//   }
 }
