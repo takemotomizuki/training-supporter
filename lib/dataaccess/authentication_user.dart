a6068d0d123e8c66fa5eb3351c57edff25ead125
kawasakiyohei <yohei_green@keio.jp>
2024-07-21 11:12:55 +0900

    サインアップの実装

--- lib/dataaccess/authentication_user.dart
+++ lib/dataaccess/authentication_user.dart
@@ -7,6 +7,35 @@ class AuthenticationUser {
 
   static int userId = -1;
 
+  static Future<bool> signup(String email, String password, String passwordCheck) async {
+
+    //login.dartから送信された情報を表示
+    print(email);
+    print(password);
+    print(passwordCheck);
+    if(!(password.compareTo(passwordCheck) == 0)) {
+      return false;
+    }
+
+    try {
+      // メール/パスワードでユーザー登録
+      final FirebaseAuth auth = FirebaseAuth.instance;
+      await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      print("success");
+      return true;
+
+    } catch (e) {
+      // ユーザー登録に失敗した場合
+      print(e);
+      return false;
+    }
+
+  }
+
 
 
   static Future<bool> authenticate(String email, String password) async {
